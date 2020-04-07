65d06ab5bb642ee6b0e66814a2a7511e02a0d995
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-07 16:05:10 +0200

    script - inserimento messaggio nella chat

--- js/main.js
+++ js/main.js
@@ -2,18 +2,42 @@
 
 $(document).ready(function(){
 
+  var inputChat, textInput;
+
+
+  inputChat = $('input[name="input_text"]'); // salvo in una variabile il riferimento all'input
+  inputChat.val(""); // imposto di default il valore vuoto per l'input
+  console.log(inputChat);
+
   // quando l'input per inviare un messaggio prende il focus scompare l'icona del microfono e appare l'icona per inviare il messaggio
-  $('input[name="input_text"]').focus(
-    function(){
+  inputChat.click(
+    function(e){
       $('.microphone').hide();
       $('.plane').show();
+      e.stopPropagation();
   });
 
-  // quando l'input per inviare un messaggio perde il focus scompare l'icona dell'invio messaggio e ricompare l'icona del microfono
-  $('input[name="input_text"]').blur(
+
+  // inputChat.blur(
+  //   function(){
+  //     $('.plane').hide();
+  //     $('.microphone').show();
+  // });
+
+  // quando clicco da qualsiasi parte l'icona dell'invio messaggio scompare e ricompare l'icona del microfono
+  $(document).click(
     function(){
       $('.plane').hide();
       $('.microphone').show();
+  });
+
+  // quando l'utente clicca per inviare il messaggio mi salvo il valore che l'utente ha inserito nell'input dell'invio del messaggio
+
+  $('.plane').click(
+    function(){
+      textInput = inputChat.val(); // salvo il valore dell'input in una variabile
+      $('.content-right').append("<div class='chat-send'>" + textInput + "</div>");
+      inputChat.val(""); // il valore dell'input si azzera dopo che è stato inviato il messaggio
   });
 
 
