42ff303356e78b056a70009033f3c0011086f322
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-08 11:09:02 +0200

    script - risposta del computer al messaggio dell'utente

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde. Quando clicco sull’input e quindi il cursore è pronto a scrivere per l’inserimento messaggio, l’icona cambia.
 
+// step 2
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
 $(document).ready(function(){
 
   var inputChat, textInput;
@@ -36,8 +40,16 @@ $(document).ready(function(){
         // altrimenti inseriscilo nella finestra della conversazione
       } else {
           $('.content-right').append("<div class='chat chat-send'>" + "<p class='text-message'>" + textInput + "</p>" +  "<span class='message-time'>13:42</span>" + "</div>");
+
           inputChat.val(""); // il valore dell'input si azzera dopo che è stato inviato il messaggio
+
+          // timing function che manda il messaggio "ok" in risposta al messaggio dell'utente dopo 1s che l'utente ha scritto
+          setTimeout(
+            function () {
+              $('.content-right').append("<div class='chat chat-receveid'>" + "<p class='text-message'>" + "ok" + "</p>" +  "<span class='message-time'>13:43</span>" + "</div>");
+          }, 1000);
       }
   });
+
 
 });
